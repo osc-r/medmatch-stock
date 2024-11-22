ec317cb6fd37cffc55ae5a415b5758b05e3876a3
oscar <osx@Oscars-MacBook-Pro.local>
2024-11-22 20:35:06 +0700

    comment unused

--- src/app/(authorized)/stock-availability/page.tsx
+++ src/app/(authorized)/stock-availability/page.tsx
@@ -21,15 +21,15 @@ import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Select from "@/components/Select";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-const BarcodeScanner = dynamic(
-  () => {
-    import("react-barcode-scanner/polyfill");
-    return import("react-barcode-scanner").then((mod) => mod.BarcodeScanner);
-  },
-  { ssr: false }
-);
+// const BarcodeScanner = dynamic(
+//   () => {
+//     import("react-barcode-scanner/polyfill");
+//     return import("react-barcode-scanner").then((mod) => mod.BarcodeScanner);
+//   },
+//   { ssr: false }
+// );
 
 const ITEMS = [
   {
