2fd2e11448fbedadf44501a8cff8002a793da15c
oscar <osx@Oscars-MacBook-Pro-2.local>
2024-12-14 22:05:11 +0700

    fix build failed

--- src/services/location/location.service.ts
+++ src/services/location/location.service.ts
@@ -1,22 +1,19 @@
-import Client from "../client";
-import { BaseResponse } from "../type";
-
 class LocationService {
-//   getLocations() {
-//     //limit, page, search
-//     return Client.instance.post<BaseResponse<LoginResponse>>("/v2/locations");
-//   }
-//   getActiveLocationsByProductId(productId: string) {
-//     //productId
-//     return Client.instance.post<BaseResponse<LoginResponse>>(
-//       "/v2/locations/active"
-//     );
-//   }
-//   getLocationsByProductId(productId: string) {
-//     return Client.instance.post<BaseResponse<LoginResponse>>(
-//       "/v1/packaging/location/$medicineId"
-//     );
-//   }
+  //   getLocations() {
+  //     //limit, page, search
+  //     return Client.instance.post<BaseResponse<LoginResponse>>("/v2/locations");
+  //   }
+  //   getActiveLocationsByProductId(productId: string) {
+  //     //productId
+  //     return Client.instance.post<BaseResponse<LoginResponse>>(
+  //       "/v2/locations/active"
+  //     );
+  //   }
+  //   getLocationsByProductId(productId: string) {
+  //     return Client.instance.post<BaseResponse<LoginResponse>>(
+  //       "/v1/packaging/location/$medicineId"
+  //     );
+  //   }
 }
 
 const locationService = new LocationService();
